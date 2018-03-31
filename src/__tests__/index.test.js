3f98e71b539884c4d4a0db02812ebf12777308ea
Matt Hamlin <hamlim@outlook.com>
2018-03-31 14:49:01 -0400

    Next (#9)
    * Setup jest for unit tests
    
    * Breaking changes: Refactor to accept mediaQueryFactory functions for generating custom breakpoint resolvers
    
    * Rename getMQValue to be specific to the min-width BP implementation
    
    * Update docs

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,59 @@
+import resp, {
+  minWidthQueryFactory,
+  getWidthValue,
+} from '../index.js'
+
+const breakpoints = {
+  small: 360,
+  medium: 800,
+  large: 1200,
+}
+
+const breakpointNames = Object.keys(breakpoints)
+
+test('it returns an object of functions named the same as the breakpoints', () => {
+  const breakpointHandlers = resp(breakpoints)
+  expect(Object.keys(breakpointHandlers)).toEqual(
+    expect.arrayContaining(breakpointNames),
+  )
+  expect(
+    typeof breakpointHandlers[breakpointNames[0]],
+  ).toBe('function')
+})
+
+test('calling a handler returns the expected mq string', () => {
+  const breakpointHandlers = resp(breakpoints)
+  const expected = `@media screen and (min-width: 50rem) { color: red; }`
+  const result = breakpointHandlers.medium`color: red;`
+  expect(result).toMatchSnapshot()
+})
+
+test('accepts a media query factory argument', () => {
+  const breakpointHandlers = resp(breakpoints, {
+    mediaQueryFactory: ({ breakpoint }) => (
+      strings,
+      ...interpolations
+    ) => {
+      return {
+        [`@media screen and (min-width: ${getWidthValue({
+          breakpoint,
+          useRems: false,
+        })})`]: strings,
+      }
+    },
+  })
+  const expectedKey = '@media screen and (min-width: 360px)'
+  const expected = { [expectedKey]: { color: 'red' } }
+  const result = breakpointHandlers.small({ color: 'red' })
+  expect(Object.keys(result)[0]).toMatchSnapshot()
+  expect(typeof result).toBe('object')
+})
+
+test('minWidthQueryFactory returns a function when called initial params', () => {
+  const mediaQuery = minWidthQueryFactory({
+    breakpoint: 300,
+    fontSize: 16,
+    useRems: true,
+  })
+  expect(typeof mediaQuery).toBe('function')
+})
