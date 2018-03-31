ddf3701e2eadccdbcd0f3c25e0b1e3df88f724ad
Matt Hamlin <hamlim@outlook.com>
2018-03-30 21:23:48 -0400

    Setup jest for unit tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,6 +1,4 @@
-import resp from './index.js'
-
-console.log('Starting Tests...')
+import resp from '../index.js'
 
 const breakpoints = {
   small: 360,
@@ -8,39 +6,32 @@ const breakpoints = {
   large: 1200,
 }
 
-const test = (name, func) => {
-  console.log(`Running ${name}...`)
-  try {
-    func()
-    console.log(`✅ ${name} passed.`)
-  } catch (e) {
-    console.error(`❌ ${name} failed.`)
-    console.error(e)
-  }
-}
-
 const breakpointNames = Object.keys(breakpoints)
 
 test('it returns an object of functions named the same as the breakpoints', () => {
   const breakpointHandlers = resp(breakpoints)
-  Object.keys(breakpointHandlers).forEach((handler, index) => {
-    console.assert(handler === breakpointNames[index])
-    console.assert(typeof breakpointHandlers[handler] === 'function')
-  })
+  expect(Object.keys(breakpointHandlers)).toEqual(
+    expect.arrayContaining(breakpointNames),
+  )
+  expect(
+    typeof breakpointHandlers[breakpointNames[0]],
+  ).toBe('function')
 })
 
 test('calling a handler returns the expected mq string', () => {
   const breakpointHandlers = resp(breakpoints)
   const expected = `@media screen and (min-width: 50rem) { color: red; }`
   const result = breakpointHandlers.medium`color: red;`
-  console.assert(result === expected)
+  expect(result).toMatchSnapshot()
 })
 
 test('accepts addition argument to configure return types', () => {
-  const breakpointHandlers = resp(breakpoints, { useRems: false })
+  const breakpointHandlers = resp(breakpoints, {
+    useRems: false,
+  })
   const expectedKey = '@media screen and (min-width: 360px)'
   const expected = { [expectedKey]: { color: 'red' } }
   const result = breakpointHandlers.small({ color: 'red' })
-  console.assert(Object.keys(result)[0] === expectedKey)
-  console.assert(typeof result === 'object')
+  expect(Object.keys(result)[0]).toMatchSnapshot()
+  expect(typeof result).toBe('object')
 })
